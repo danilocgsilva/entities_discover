6475220b8351a4a61d494e1818233d6a3822104f
Danilo Silva <contato@danilocgsilva.me>
2023-02-07 13:28:28 +0000

    Added test template

---
+++ tests/EntityTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Danilocgsilva\EntitiesDiscover\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class EntityTest extends TestCase
+{
+    public function testGetForeigns()
+    {
+        $this->assertSame(1, 2);
+    }
+}
